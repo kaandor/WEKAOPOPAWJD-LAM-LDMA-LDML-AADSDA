6ce788badcc3fe964eb2b35c6895b5ab45f73df1
Kaandro Becker <kaandro@example.com>
2026-01-26 20:29:10 -0300

    Add Service Worker registration

--- js/pwa.js
+++ js/pwa.js
@@ -167,3 +167,14 @@ if (document.readyState === 'loading') {
 } else {
   createInstallPrompt();
 }
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
