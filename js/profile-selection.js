f47ad59947b2b37c0a4acac7a13e5b4f81694896
Kaandro Becker <kaandro@klyx.app>
2026-01-31 01:22:50 -0300

    Fix movies.js path, remove logo.svg ref, bump versions

--- js/profile-selection.js
+++ js/profile-selection.js
@@ -1,4 +1,4 @@
-import { api } from "./api.js?v=20240130";
+import { api } from "./api.js?v=20260131";
 import { requireAuth } from "./auth.js";
 
 // Ensure user is logged in
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'klyx-cache-v7';
+const CACHE_NAME = 'klyx-cache-v8';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
